69b1f5c50c2463706f8396b025a57e65a5088a85
RonRon <ron.lambojon@gmail.com>
2019-11-20 11:35:57 +0800

    Updates

---
+++ precache-manifest.6a7b2c8092021d266b81c3cb70ebc6b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236daa87d13a7d3a15ea6f79e634a843",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dc108b7379a0dc06f78c",
+    "url": "/monsters-rolodex/static/css/main.9a1e094c.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "dc108b7379a0dc06f78c",
+    "url": "/monsters-rolodex/static/js/main.687e5848.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
